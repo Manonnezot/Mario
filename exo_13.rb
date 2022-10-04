1c7c051cedb64ef2a57d2b6905f01a8fdb74679d
Manonn <manon.nezot@gmail.com>
2022-10-04 18:32:05 +0200

    exo_13

---
+++ exo_13.rb
@@ -0,0 +1,13 @@
+table = []
+
+50.times {
+    |i| number = i + 1
+    table.append("jean.dupont#{"%02d" % number}@email.fr") }
+
+puts table
+
+
+#créer 50 faux emails
+    #format "jean.dupont.01@email.fr"
+# mettre dans un array
+
