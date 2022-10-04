fa89c04830e817d410ad8951dc5dc874d70fe67d
Manonn <manon.nezot@gmail.com>
2022-10-04 18:46:13 +0200

    exo_14

---
+++ exo_14.rb
@@ -0,0 +1,10 @@
+table = []
+
+50.times {
+    |i| number = i + 1
+    table.append("jean.dupont#{"%02d" % number}@email.fr") }
+    for i in 0..50
+        if i % 2 !=0
+        puts "#{table[i]}"
+        end
+    end
